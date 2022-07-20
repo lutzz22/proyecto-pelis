13f2b4eff9f82cd2157f5195b4979dd973932279
luaramb <arambarrilucia@gmail.com>
2022-07-19 21:57:29 -0300

    error fix

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -17,7 +17,7 @@ const {contador} = useContext(Context)
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                    <Link className="nav-link active link-light" aria-current="page" to="/">Inicio</Link>
+                    <Link className="nav-link active link-light" aria-current="page" to="/proyecto-pelis">Inicio</Link>
                     </li>
                     <li className="nav-item">
                     <Link className="nav-link link-light" to="/Peliculas">Peliculas</Link>
